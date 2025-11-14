3d3a3419029a9a24748f9e9c0d2e6e334566cae8
Einsteinke-hub <einstenmarto30@gmail.com>
2025-11-14 22:10:55 +0300

    updated

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -105,12 +105,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Django REST Framework configuration
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-    ],
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.JSONParser',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
     ],
 }
