68e9fa608fa91760d183202df8077a8759b1b56a
Einsteinke-hub <einstenmarto30@gmail.com>
2025-11-28 15:36:58 +0300

    made changes

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,12 +1,13 @@
-"""
-URL configuration for messaging_app project.
-"""
-
-from django.contrib import admin
+# messaging_app/urls.py
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # Include the chats app API URLs
-    path('api-auth/', include('rest_framework.urls')),  # For browsable API auth
+    # ... other URLs ...
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/chats/', include('chats.urls')),
 ]
